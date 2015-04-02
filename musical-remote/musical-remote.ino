b0b9ecb6fa9c1853f7dcb740423305d86d5432bc
Austin Jackson <themacphage@gmail.com>
2015-04-02 10:24:04 -0500

    wow, we left the tutorial stuff

--- musical-remote/musical-remote.ino
+++ musical-remote/musical-remote.ino
@@ -2,20 +2,13 @@
 #include <Tone.h>
 
 /*
-  Melody
+  Musical Remote
+  
+  by Austin Jackson, Jose Del Villar, and Andrew Wallace
+  of
+  Reagan High School in Houston
  
- Plays a melody 
- 
- circuit:
- * 8-ohm speaker on digital pin 8
- 
- created 21 Jan 2010
- modified 30 Aug 2011
- by Tom Igoe 
- 
- This example code is in the public domain.
- 
- http://arduino.cc/en/Tutorial/Tone
+ https://github.com/au5ton/arduino-musical-remote
  
  */
 //colors
@@ -54,26 +47,6 @@ int song_melody[] = {
 int song_tempo[] = {
   4, 8, 8, 4, 4, 4, 4, 4};
 
-void playSong(int s) {
-
-  // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-    // to calculate the note duration, take one second 
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000/song_tempo[thisNote];
-    //tone(8, song_melody[thisNote],noteDuration);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    //noTone(8);
-  }
-
-}
 Tone buzzer; //The pietzo object used for producing sound
 int currentColor [] = {
   0,0,0}; //Current color written to the RGB LED
